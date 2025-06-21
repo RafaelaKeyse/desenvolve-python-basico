7cac594e21bd7f21ef5a019b4d2e7eb49a95674f
RafaelaKeyse <rafaelakeyse.pdbd217@pdbomdespacho.com.br>
2025-06-21 10:06:29 -0300

    Aula 2.2 - Tipos de dados

---
+++ Módulo 2- Fundamentos/aula2_questao4.py
@@ -0,0 +1,12 @@
+# Questão 4
+
+juros = 1.01
+saldo = 500.0
+
+# Usando só duas variáveis para 3 meses
+saldo = saldo * juros  # 1º mês
+saldo = saldo * juros  # 2º mês
+saldo = saldo * juros  # 3º mês
+
+print("Após 3 meses meu novo saldo é")
+print(saldo)
